f4a076ad350f5390a589a40745515fb004f360e3
sebfried <dev@sebastianfriedrich.at>
2023-07-24 17:35:03 +0200

    cleanup

--- source/js/index.js
+++ source/js/index.js
@@ -1,30 +1,81 @@
-var images = document.getElementsByTagName('img');
-var lastScrollY = window.scrollY;
-var ticking = false;
+var images = document.getElementsByClassName('art');
+var artContainer = document.getElementById('artcontainer');
+var artContainerWidth = artContainer.clientWidth;
+var fixedWindowHeight = window.innerHeight;
 
 function update() {
-  var currentScrollY = window.scrollY;
-  var delta = currentScrollY - lastScrollY;
+  var fullSizePosition = 100;
+  var actionArea = fixedWindowHeight - fullSizePosition;
 
   for (var i = 0; i < images.length; i++) {
     var image = images[i];
-    var position = image.getBoundingClientRect();
-    if (position.top < window.innerHeight && position.bottom >= 0) {
-      var percent = (position.top + position.height / 2) / (window.innerHeight / 2);
-      image.style.width = (200 + percent * 200) + 'px';
-      image.style.filter = 'grayscale(' + (100 - percent * 100) + '%)';
+    var imagePosition = image.getBoundingClientRect();
+
+    if (imagePosition.top < window.innerHeight && imagePosition.bottom >= 0 && imagePosition.top >= fullSizePosition) {
+
+      // percent to fullSizePosition
+      var multiply = (imagePosition.top - fullSizePosition) / actionArea;
+
+      // Calculate the new width and grayscale value based on the inverted percentage from the middle
+      const newWidth = 100 * (1 - multiply);
+      const grayscaleValue = 100 * multiply;
+
+      // Set the new styles for the image
+      image.style.width = newWidth + '%';
+      // image.style.filter = 'grayscale(' + grayscaleValue + '%)';
+    } else {
+      image.style = {};
     }
   }
-
-  lastScrollY = currentScrollY;
-  ticking = false;
 }
 
-function requestTick() {
-  if (!ticking) {
-    requestAnimationFrame(update);
-    ticking = true;
+function hideElement(element) {
+  element.style.opacity = 0;
+}
+
+// Function to create the fade-in effect for a given element
+function fadeIn(element, duration) {
+  var start = 0;
+  var end = 1;
+  var current = start;
+  var increment = 0.01; // Adjust this value for smoother or faster fading
+
+  element.style.opacity = start;
+
+  var timer = setInterval(function () {
+    current += increment;
+    element.style.opacity = current;
+
+    if (current >= end) {
+      clearInterval(timer);
+    }
+  }, duration / (1 / increment));
+}
+
+function fixViewportHeight() {
+  fixedWindowHeight = window.innerHeight;
+}
+
+function handleResize() {
+  // Check if the width has changed since the last resize event
+  if (window.innerWidth !== handleResize.prevWidth) {
+    // Width has changed, do your resize-related tasks here
+    fixViewportHeight();
+    update();
+    console.log("Reszize");
+
+    // Update the previous width value to the current width
+    handleResize.prevWidth = window.innerWidth;
   }
 }
 
-window.addEventListener('scroll', requestTick);
+// Initialize the previous width value with the current width
+handleResize.prevWidth = window.innerWidth;
+
+(function initialize() {
+  hideElement(artContainer);
+  window.addEventListener('load', update, fadeIn(artContainer, 1000));
+})();
+
+window.addEventListener('scroll', update);
+window.addEventListener('resize', handleResize);
