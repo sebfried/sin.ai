bf714fe0af3381260afebd9413254e93215c3e1d
sebfried <dev@sebastianfriedrich.at>
2023-07-24 21:52:04 +0200

    cleanup

--- source/js/main.js
+++ source/js/main.js
@@ -3,7 +3,7 @@
   window.onload = function() {
     'use strict';
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./sw.js').then(function(registration) {
+      navigator.serviceWorker.register('./pwa/sw.js').then(function(registration) {
         console.log('Service Worker Registered', registration);
       }).catch(function(err) {
         console.log('Service Worker Failed to Register', err);
