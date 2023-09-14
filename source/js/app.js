0be8f9bb564b53d852516a0197077ffb243b7e06
sebfried <dev@sebastianfriedrich.at>
2023-09-14 15:25:02 +0200

    fix preloader and sticky header glitch

--- source/js/app.js
+++ source/js/app.js
@@ -1,8 +1,25 @@
+// Wait for the page to load before running the animation
+window.addEventListener('load', function () {    
+    // Use GSAP to create an animation
+    gsap.to('.preloader', {
+      opacity: 0, // Change the opacity to 0
+      duration: 1, // Set the duration of the animation
+      ease: 'power2.out', // Use an easing function (ease out in this case)
+      onComplete: function () {
+        gsap.to('.preloader', { display: 'none' });
+        gsap.to('.content', { duration: 1, opacity: 1, y: -20, ease: 'power2.out' });
+      }
+    });
+  });
 
-// GSAP Animations
-window.addEventListener('load', function () {
-    gsap.to('.preloader', { duration: 1, opacity: 0, display: 'none', ease: 'power1.out' });
 
-    // Start the introduction animation once the preloader is hidden
-    gsap.to('.content', { duration: 1, opacity: 1, y: -50, ease: 'power2.out', delay: 1 });
-});
+// // GSAP Animations
+// window.addEventListener('load', function () {
+//     gsap.to('.preloader', { duration: 2, opacity: 0, overwrite: "all", ease: 'power1.out' });
+
+//     // Add delay before hiding the preloader
+//     gsap.to('.preloader', { delay: 2, display: 'none' });
+
+//     // Start the introduction animation once the preloader is hidden
+//     gsap.to('.content', { duration: 1, opacity: 1, y: -50, ease: 'power2.out', delay: 4 });
+// });
