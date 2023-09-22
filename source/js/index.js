4680160614d752777c6d16b1f06a2c17ffee270c
sebfried <dev@sebastianfriedrich.at>
2023-09-22 13:31:24 +0200

    cleanup

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,3 @@
-// Wait for the document to be ready
 document.addEventListener("DOMContentLoaded", function () {
     initImageScrollAnimations();
     initImageClicks();
@@ -6,17 +5,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function initImageScrollAnimations() {
-    // Get all the images in the column
     const images = document.querySelectorAll(".image-column > div");
 
-    // Loop through each image and create the scroll animation
     images.forEach((image) => {
         createScrollAnimation(image);
     });
 }
 
 function createScrollAnimation(image) {
-    // Use GSAP to animate the image
     gsap.fromTo(
         image,
         {
@@ -85,14 +81,12 @@ function handleLinkClick(event) {
 function collapseImageInfo(link) {
     const imageInfo = link.parentNode.querySelector(".image-info");
 
-    // Use GSAP to animate the collapse
     gsap.to(imageInfo, {
         height: 0,
         opacity: 0,
-        duration: 0.5, // Adjust the animation duration as needed
+        duration: 0.5, 
         onComplete: () => {
-            imageInfo.style.display = 'none'; // Hide the element after the animation
-            // After opening or closing an accordion
+            imageInfo.style.display = 'none';
             ScrollTrigger.refresh();
         }
     });
@@ -109,15 +103,13 @@ async function expandImageInfo(link) {
             parentElement.appendChild(fetchedContent.cloneNode(true));
 
             const imageInfo = parentElement.querySelector('.image-info');
-            imageInfo.style.display = 'block'; // Ensure the element is visible
+            imageInfo.style.display = 'block';
 
-            // Use GSAP to animate the expand
             gsap.to(imageInfo, {
                 height: 'auto',
                 opacity: 1,
-                duration: 0.5, // Adjust the animation duration as needed
+                duration: 0.5, 
                 onComplete: () => {
-                    // After opening or closing an accordion
                     ScrollTrigger.refresh();
                 }
             });
@@ -139,7 +131,6 @@ async function fetchAndInsertContent(pathToPage, thisElement) {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-
         const html = await response.text();
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
@@ -156,85 +147,3 @@ function giveTouchTooltip() {
         // window.alert("touch!");
     }
 }
-
-
-
-
-
-//
-// Functions
-//
-
-// Complex Lazy Loading
-
-// // Remove HTML Lazy Loading
-// // HTML Lazy Loading is only used for browsers without JavaScript. 
-// // If JavaScript is enabled, we use Lazy Loading with JavaScript.
-// function removeHtmlLazyLoading() {
-//     const lazyImages = document.querySelectorAll('img[loading="lazy"]');
-//     for (const img of lazyImages) {
-//         img.removeAttribute('loading');
-//     }
-// }
-
-// // Prepare Images for Lazy Loading
-// function srcToDataSrc() {
-//     const lazyImages = document.querySelectorAll('.lazy img');
-//     for (const img of lazyImages) {
-//         const src = img.getAttribute('src');
-//         img.setAttribute('date-src', src);
-//         img.removeAttribute('src');
-//     }
-// }
-
-// // Load Image
-// function loadImage(img) {
-//     const src = img.getAttribute('data-src');
-//     img.setAttribute('src', src);
-// }
-
-// // Function to handle lazy loading
-// function lazyLoadImage(image) {
-//     if (image.dataset.src) {
-//         image.src = image.dataset.src;
-//         image.removeAttribute('data-src');
-//     }
-// }
-
-// // Lazy Loading Images
-// document.addEventListener('DOMContentLoaded', function () {
-//     const images = document.querySelectorAll('.lazy img');
-
-//     // Intersection Observer configuration
-//     const observerOptions = {
-//         rootMargin: '0px 0px -1000px 0px', // Load when the image is 1000px away from the bottom edge of the viewport
-//         threshold: 0 // Set to 0 to ensure the callback is fired as soon as the element enters the viewport
-//     };
-
-//     const observer = new IntersectionObserver((entries, observer) => {
-//         entries.forEach((entry) => {
-//             if (entry.isIntersecting) {
-//                 const img = entry.target;
-//                 const overlay = img.previousElementSibling;
-
-//                 // Start preloader fade-out animation using GSAP
-//                 gsap.to(overlay, {
-//                     duration: 1, opacity: 0, onComplete: () => {
-//                         overlay.style.display = 'none'; // Hide the overlay once the animation is done
-//                     }
-//                 });
-
-//                 img.src = img.getAttribute('data-src'); // Load the image
-//                 observer.unobserve(img); // Stop observing the image once it's loaded
-//             }
-//         });
-//     }, observerOptions);
-
-//     // Start observing each lazy-loaded image
-//     images.forEach((img) => {
-//         observer.observe(img);
-//     });
-// });
-
-
-
