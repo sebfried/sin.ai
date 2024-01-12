c77be16c75dec6677a353b4be738e060d7ee3bf6
sebfried <dev@sebastianfriedrich.at>
2024-01-12 02:06:10 +0100

    elegantify

--- scripts/start-copy.js
+++ scripts/start-copy.js
@@ -4,18 +4,15 @@ const path = require('path');
 // temporary solution till fs-extra
 
 const assetsToCopy = [
-  { source: 'node_modules/gsap/dist/gsap.min.js', destination: 'source/vendor/gsap.min.js' },
-  { source: 'node_modules/gsap/dist/ScrollTrigger.min.js', destination: 'source/vendor/ScrollTrigger.min.js' },
-  { source: 'node_modules/gsap/dist/Flip.min.js', destination: 'source/vendor/Flip.min.js' },
+  { source: './node_modules/gsap/dist/gsap.min.js', destination: './source/vendor/gsap.min.js' },
+  { source: './node_modules/gsap/dist/ScrollTrigger.min.js', destination: './source/vendor/ScrollTrigger.min.js' },
+  { source: './node_modules/gsap/dist/Flip.min.js', destination: './source/vendor/Flip.min.js' },
   // Add more assets to copy here if needed
 ];
 
 function copyAssets() {
   assetsToCopy.forEach((asset) => {
-    const sourcePath = path.join(__dirname, '..', asset.source);
-    const destinationPath = path.join(__dirname, '..', asset.destination);
-
-    fs.copyFile(sourcePath, destinationPath, (err) => {
+    fs.copyFile(asset.source, asset.destination, (err) => {
       if (err) {
         console.error(`Error copying ${asset.source} to ${asset.destination}:`, err);
       } else {
@@ -26,3 +23,5 @@ function copyAssets() {
 }
 
 copyAssets();
+
+//TODO: test
