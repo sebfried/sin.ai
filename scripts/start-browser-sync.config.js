573dbbc0328a06e2d3b2bdd3f64626a967e2fed4
sebfried <dev@sebastianfriedrich.at>
2024-02-08 18:35:07 +0100

    browser-sync, watch css

--- scripts/start-browser-sync.config.js
+++ scripts/start-browser-sync.config.js
@@ -5,7 +5,11 @@ module.exports = {
           extensions: ["html", "htm"]
       }
     },
-    files: ['source/*.html','source/js/*.js'],
+    files: [
+      'source/*.html',
+      'source/js/*.js',
+      'source/css/*.css'
+    ],
     https: true,
     startPath: '',
     port: 3000
